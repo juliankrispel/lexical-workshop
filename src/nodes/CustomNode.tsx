03918a5f53790d7be25e818e97f44f43cbac4824
Julian Krispel-Samsel <julian@jkrsp.com>
2022-07-13 11:32:46 +0100

    incomplete boilerplate for decorator

--- src/nodes/CustomNode.tsx
+++ src/nodes/CustomNode.tsx
@@ -1,4 +1,33 @@
-import { EditorConfig, ElementNode, LexicalEditor, SerializedElementNode } from "lexical";
+import { DecoratorNode, EditorConfig, ElementNode, LexicalEditor, LexicalNode, SerializedElementNode, SerializedLexicalNode } from "lexical";
+
+export class CollapseButton extends DecoratorNode<JSX.Element> {
+  static getType(): string {
+    return "collapser";
+  }
+
+  static importJSON(_serializedNode: SerializedLexicalNode): LexicalNode {
+    return new CollapseButton();
+  }
+
+  createDOM(_config: EditorConfig, _editor: LexicalEditor): HTMLElement {
+    const el = document.createElement("btn");
+    const className = _config.theme.collapser;
+    if (className != null) {
+      el.className = className;
+    }
+
+    return el;
+  }
+
+  updateDOM(
+    _prevNode: unknown,
+    _dom: HTMLElement,
+    _config: EditorConfig
+  ): boolean {
+    return false;
+  }
+
+}
 
 export class CustomNode extends ElementNode {
   static getType() {
